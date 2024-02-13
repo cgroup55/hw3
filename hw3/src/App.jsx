31a61b4151bd421938c5ca4d7bac36aa7bcb4cb2
bararad <proj.cgroup55@gmail.com>
2024-02-13 10:01:58 +0200

    b

--- hw3/src/App.jsx
+++ hw3/src/App.jsx
@@ -2,13 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import EditDetails from './FComps/EditDetails'
+import Login from './FComps/Login'
+import Profile from './FComps/Profile'
+import Register from './FComps/Register'
+import SystemAdmin from './FComps/SystemAdmin'
 
 function App() {
+  //set state route
   
 
   return (
     <>
-      
+    <EditDetails />
+    <Login />
+    <Profile/>
+    <Register/>
+    <SystemAdmin/>
+    
     </>
   )
 }
---
+++ hw3/src/FComps/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Profile() {
+
+    //hooks
+
+    //funcs 
+
+  return (
+    <div>Profile</div>
+  )
+}
